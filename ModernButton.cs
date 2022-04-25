6e1418af36ba6fb11d0061b913a2b2bd00105cda
Av32000 <ae.av32000@gmail.com>
2022-04-25 22:02:15 +0200

    Reformat

--- ModernButton.cs
+++ ModernButton.cs
@@ -1,43 +1,100 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.ComponentModel;
 
-namespace Calendar.CustomButtons
+namespace Calendar.CustomComponents
 {
     internal class ModernButton : Button
     {
-        private Color _borderColor = Color.FromArgb(200,200,200);
-        private Color _backgroundColor = Color.FromArgb(66, 226, 184);
+        private Color borderColor = Color.FromArgb(0, 122, 204);
+        private int borderSize = 0;
+        private int borderRadius = 40;
 
         [Category("Modern Appearance")]
-        public Color BorderColor { get => _borderColor; set { _borderColor = value; this.Invalidate(); } }
+        public Color BorderColor { get => borderColor; set { borderColor = value; this.Invalidate(); } }
         [Category("Modern Appearance")]
-        public Color BackgroundColor { get => _backgroundColor; set { _backgroundColor = value; this.Invalidate(); } }
+        public int BorderSize { get => borderSize; set { borderSize = value; this.Invalidate(); } }
+        [Category("Modern Appearance")]
+        public int BorderRadius { get => borderRadius; set { if (value <= this.Height) borderRadius = value; else BorderRadius = this.Height; this.Invalidate(); } }
+        [Category("Modern Appearance")]
+        public Color BackgroundColor { get => this.BackColor; set { this.BackColor = value; this.Invalidate(); } }
+        [Category("Modern Appearance")]
+        public Color TextColor { get => this.ForeColor; set { this.ForeColor = value; this.Invalidate(); } }
+        
+        public ModernButton()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.Size = new Size(150, 40);
+            this.BackColor = Color.FromArgb(0, 122, 204);
+            this.ForeColor = Color.White;
+            this.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            this.Cursor = Cursors.Hand;
+            this.FlatAppearance.MouseDownBackColor = Color.Transparent;
+        }
+
+        private GraphicsPath GetFigurePath(RectangleF rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.Width-radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.Width-radius, rect.Height-radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+
+            return path;
+        }
 
         protected override void OnPaint(PaintEventArgs pevent)
         {
+            base.OnPaint(pevent);
             pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
-            pevent.Graphics.Clear(this.Parent.BackColor);
 
-            pevent.Graphics.FillRectangle(new SolidBrush(_backgroundColor), this.ClientRectangle);
-            pevent.Graphics.DrawRectangle(new Pen(this._borderColor), new Rectangle(0, 0, this.Width - 1, this.Height - 1));
+            RectangleF rectSurface = new RectangleF(0, 0, this.Width, this.Height);
+            RectangleF rectBorder = new RectangleF(1, 1, this.Width - 0.8f, this.Height - 0.8f);
+            
+            if(borderRadius > 2)
+            {
+                using (GraphicsPath pathSurface = GetFigurePath(rectSurface, borderRadius))
+                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius-1))
+                using (Pen penSurface = new Pen(this.Parent.BackColor, 2))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    penBorder.Alignment = PenAlignment.Inset;
+                    this.Region = new Region(pathSurface);
 
-            TextFormatFlags flags = TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter | TextFormatFlags.WordBreak;
-            TextRenderer.DrawText(pevent.Graphics, this.Text, this.Font, this.ClientRectangle, this.ForeColor, flags);
+                    pevent.Graphics.DrawPath(penSurface, pathSurface);
+                    if (borderSize >= 1)
+                        pevent.Graphics.DrawPath(penBorder, pathBorder);
+                    
+                }
+            }
+            else
+            {
+                this.Region = new Region(rectSurface);
+                if (borderSize >= 1)
+                {
+                    using (Pen penBorder = new Pen(borderColor, borderSize))
+                    {
+                        penBorder.Alignment = PenAlignment.Inset;
+                        pevent.Graphics.DrawRectangle(penBorder, 0,0, this.Width-1, this.Height-1);
+                    }
+                }
+            }
         }
 
-        public ModernButton()
+        protected override void OnHandleCreated(EventArgs e)
         {
-            this.ForeColor = Color.White;
-            this.Cursor = Cursors.Hand;
+            base.OnHandleCreated(e);
+            this.Parent.BackColorChanged += new EventHandler(Container_BackColorChanged);
+        }
+
+        private void Container_BackColorChanged(object sender, EventArgs e)
+        {
+            if (this.DesignMode) this.Invalidate();
         }
     }
-
-    
 }
